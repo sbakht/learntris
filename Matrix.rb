6a59fa6695e2080ddb0135625d54316575ffac39
Saad Bakht <rotk13@gmail.com>
2014-07-26 23:24:25 -0400

    I rotation completed, and O by default

--- Matrix.rb
+++ Matrix.rb
@@ -3,12 +3,24 @@ class Matrix
 	@score = 0
 	@numCleared = 0
 	@activeTetramino = ""
+	@letter1 = ""
+	@letter2 = ""
+	@letter3 = ""
+	@letter4 = ""
+	@rotations = 0
+	@arrTetramino = []
 
 	def initialize
 		@matrix = @matrix = ". . . . . . . . . .\n" * 22
 		@score = 0
 		@numCleared = 0
 		@activeTetramino = ""
+		@letter1 = ""
+		@letter2 = ""
+		@letter3 = ""
+		@letter4 = ""
+		@rotations = 0
+		@arrTetramino = []
 	end
 
 	def setCleared
@@ -34,7 +46,7 @@ class Matrix
 		@matrix = arr.join("\n")
 	end
 
-	def print
+	def show
 		puts @matrix
 	end
 
@@ -56,14 +68,119 @@ class Matrix
 
 	def setActiveTetramino(char)
 		@activeTetramino = char
+		if @activeTetramino == "I"
+			@letter1 = [1,0]
+			@letter2 = [1,1]
+			@letter3 = [1,2]
+			@letter4 = [1,3]
+		end
+		if @activeTetramino == "O"
+			@letter1 = [0,0]
+			@letter2 = [1,0]
+			@letter3 = [0,1]
+			@letter4 = [1,1]
+			# puts "y y"
+			# puts "y y"
+		end
+		if @activeTetramino == "Z"
+			@letter1 = [0,0]
+			@letter2 = [1,0]
+			@letter3 = [1,1]
+			@letter4 = [2,2]
+			# puts "r r ."
+			# puts ". r r"
+			# puts ". . ."
+		end
+		if @activeTetramino == "S"
+			@letter1 = [1,0]
+			@letter2 = [2,0]
+			@letter3 = [0,1]
+			@letter4 = [1,1]
+			# puts ". g g"
+			# puts "g g ."
+			# puts ". . ."
+		end
+		if @activeTetramino == "J"
+			@letter1 = [0,0]
+			@letter2 = [0,1]
+			@letter3 = [1,1]
+			@letter4 = [2,1]
+			# puts "b . ."
+			# puts "b b b"
+			# puts ". . ."
+		end
+		if @activeTetramino == "L"
+			@letter1 = [2,0]
+			@letter2 = [0,1]
+			@letter3 = [1,1]
+			@letter4 = [2,1]
+			# puts ". . o"
+			# puts "o o o"
+			# puts ". . ."
+		end
+		if @activeTetramino == "T"
+			@letter1 = [1,0]
+			@letter2 = [0,1]
+			@letter3 = [1,1]
+			@letter4 = [2,1]
+			# puts ". m ."
+			# puts "m m m"
+			# puts ". . ."
+		end
+	end
+
+	def rotate
+		@arrTetramino = [['.','.','.','.'],['.','.','.','.'],['.','.','.','.'],['.','.','.','.']]
+		if @rotations == 0
+			@arrTetramino[@letter1[0] - 1][@letter1[1] + 2] = "c"
+			@arrTetramino[@letter1[0]][@letter1[1] + 2] = "c"
+			@arrTetramino[@letter1[0] + 1][@letter1[1] + 2] = "c"
+			@arrTetramino[@letter1[0] + 2][@letter1[1] + 2] = "c"
+			@letter1 = [@letter1[0] - 1, @letter1[1] + 2]
+			# @letter1 = [@letter1[0] - 1, @letter1[0] + 2]
+			@rotations = 1
+		elsif @rotations == 1
+			@arrTetramino[@letter1[0] + 2][@letter1[1] + 1] = "c"
+			@arrTetramino[@letter1[0] + 2][@letter1[1]] = "c"
+			@arrTetramino[@letter1[0] + 2][@letter1[1] - 1] = "c"
+			@arrTetramino[@letter1[0] + 2][@letter1[1] - 2] = "c"
+			@letter1 = [@letter1[0] + 2, @letter1[1] + 1]
+			@rotations = 2
+		elsif @rotations == 2
+			@arrTetramino[@letter1[0] + 1][@letter1[1] - 2] = "c"
+			@arrTetramino[@letter1[0]][@letter1[1] - 2] = "c"
+			@arrTetramino[@letter1[0] - 1][@letter1[1] - 2] = "c"
+			@arrTetramino[@letter1[0] - 2][@letter1[1] - 2] = "c"
+			@letter1 = [@letter1[0] + 1, @letter1[1] - 2]
+			@rotations = 3
+		elsif @rotations == 3
+			@arrTetramino[@letter1[0] - 2][@letter1[1] - 1] = "c"
+			@arrTetramino[@letter1[0] - 2][@letter1[1]] = "c"
+			@arrTetramino[@letter1[0] - 2][@letter1[1] + 1] = "c"
+			@arrTetramino[@letter1[0] - 2][@letter1[1] + 2] = "c"
+			@letter1 = [@letter1[0] - 2, @letter1[1] - 1]
+			@rotations = 0
+		end
+
 	end
 
 	def printActiveTetramino
 		if @activeTetramino == "I"
-			puts ". . . ."
-			puts "c c c c"
-			puts ". . . ."
-			puts ". . . ."
+			if @rotations == 0
+				@arrTetramino = [['.','.','.','.'],['.','.','.','.'],['.','.','.','.'],['.','.','.','.']]
+				@arrTetramino[@letter1[0]][@letter1[1]] = "c"
+				@arrTetramino[@letter2[0]][@letter2[1]] = "c"
+				@arrTetramino[@letter3[0]][@letter3[1]] = "c"
+				@arrTetramino[@letter4[0]][@letter4[1]] = "c"
+			end
+			@arrTetramino.each do |line|
+				puts line.join(' ')
+			end
+			# print arr
+			# puts ". . . ."
+			# puts ". . . ."
+			# puts "c c c c"
+			# puts ". . . ."
 		end
 		if @activeTetramino == "O"
 			puts "y y"
