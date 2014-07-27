2a47ebd922e5a06fbe75a03be346add2523a0e26
Saad Bakht <rotk13@gmail.com>
2014-07-26 20:08:51 -0400

    switched to exit because break only leaves inner loop

--- implement.rb
+++ implement.rb
@@ -3,29 +3,34 @@ require_relative "Matrix"
 matrix = Matrix.new
 
 while 
-    cmd = gets.chomp
-
-    case cmd
-    when "q"
-        break
-    when "p"
-        matrix.print
-    when "g"
-        matrix.setFromInput
-    when "c"
-        matrix.setCleared
-    when "?s"
-        matrix.printScore
-    when "?n"
-        matrix.printNumCleared
-    when "s"
-        matrix.clearBlocks
-    when "I"
-        matrix.setActiveTetramino("I")
-    when "O"
-        matrix.setActiveTetramino("O")
-    when "t"
-        matrix.printActiveTetramino
+    cmds = gets.chomp.split(" ")
+    # puts cmds
+    cmds.each do |cmd|
+        # puts cmd
+        case cmd
+        when "q"
+            exit #exits the code
+        when "p"
+            matrix.print
+        when "g"
+            matrix.setFromInput
+        when "c"
+            matrix.setCleared
+        when "?s"
+            matrix.printScore
+        when "?n"
+            matrix.printNumCleared
+        when "s"
+            matrix.clearBlocks
+        when "I"
+            matrix.setActiveTetramino("I")
+        when "O"
+            matrix.setActiveTetramino("O")
+        when "Z"
+            matrix.setActiveTetramino("Z")
+        when "t"
+            matrix.printActiveTetramino
+        end
     end
 
 end
