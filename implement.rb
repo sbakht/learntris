d43e086a9d2608f5502afb005a10683d484e6be6
Saad Bakht <rotk13@gmail.com>
2014-07-28 03:31:44 -0400

    commands can be sent without spaces

--- implement.rb
+++ implement.rb
@@ -3,45 +3,55 @@ require_relative "Matrix"
 matrix = Matrix.new
 
 while 
-    cmds = gets.chomp.split(" ")
-    # puts cmds
-    cmds.each do |cmd|
-        # puts cmd
-        case cmd
-        when "q"
-            exit #exits the code
-        when "p"
-            matrix.show
-        when "g"
-            matrix.setFromInput
-        when "c"
-            matrix.setCleared
-        when "?s"
-            matrix.printScore
-        when "?n"
-            matrix.printNumCleared
-        when "s"
-            matrix.clearBlocks
-        when "I"
-            matrix.setActiveTetramino("I")
-        when "O"
-            matrix.setActiveTetramino("O")
-        when "Z"
-            matrix.setActiveTetramino("Z")
-        when "S"
-            matrix.setActiveTetramino("S")
-        when "J"
-            matrix.setActiveTetramino("J")
-        when "L"
-            matrix.setActiveTetramino("L")
-        when "T"
-            matrix.setActiveTetramino("T")
-        when ")"
-            matrix.rotate
-        when "t"
-            matrix.show
-        when ";"
-            puts "\n"
+    input = gets.chomp.split(" ") #allows commands to be separated by spaces
+    input.each do |cmds|
+        #allows commands to be written without spaces
+        #doesn't split on 2char commands (which have a ?)
+        if !cmds.include?("?")
+            cmds = cmds.split("") 
+        else
+            cmds = [cmds] #makes it an array so it can pass in the each function
+        end
+        cmds.each do |cmd|
+            case cmd
+            when "q"
+                exit #exits the code
+            when "p"
+                matrix.show
+            when "P"
+                matrix.setCleared
+                matrix.showWithTetramino
+            when "g"
+                matrix.setFromInput
+            when "c"
+                matrix.setCleared
+            when "?s"
+                matrix.printScore
+            when "?n"
+                matrix.printNumCleared
+            when "s"
+                matrix.clearBlocks
+            when "I"
+                matrix.setActiveTetramino("I")
+            when "O"
+                matrix.setActiveTetramino("O")
+            when "Z"
+                matrix.setActiveTetramino("Z")
+            when "S"
+                matrix.setActiveTetramino("S")
+            when "J"
+                matrix.setActiveTetramino("J")
+            when "L"
+                matrix.setActiveTetramino("L")
+            when "T"
+                matrix.setActiveTetramino("T")
+            when ")"
+                matrix.rotate
+            when "t"
+                matrix.show
+            when ";"
+                puts "\n"
+            end
         end
     end
 
